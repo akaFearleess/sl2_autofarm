112bf6ae81ac1b37505873c984c224f09ca19dce
reavscripts <72871071+reavscripts@users.noreply.github.com>
2020-10-27 09:26:43 +0100

    3.5.∞^3

--- main.lua
+++ main.lua
@@ -320,14 +320,16 @@ spawn(function()
             local spins = player.statz.spins.Value
             if spins < 500 then
     		    for i,v in pairs(workspace.missiongivers:GetChildren()) do
-    		        if mission.Visible == false and v.ClassName == "Model" and v:FindFirstChild("Head"):FindFirstChild("givemission").Enabled and v:FindFirstChild("CLIENTTALK") and v:FindFirstChild("Talk") and string.find(v.Talk.talk1.Value, "TRICK") and v.Talk:FindFirstChild("typ").Value == "halloweenevent" and v.Head.givemission.color.Image == candy then
-        				repeat wait()
-            			    toTarget(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position,v.HumanoidRootPart.Position,CFrame.new(v.HumanoidRootPart.Position+Vector3.new(0,-5,0)))
-        	    			v.CLIENTTALK:FireServer()
-        	    			wait(.2)
-        		    		v.CLIENTTALK:FireServer("accept")
-        		    	until v:FindFirstChild("Head").givemission.Enabled == false or not candies
-    			    end
+    		        pcall(function()
+        		        if mission.Visible == false and v.ClassName == "Model" and v:FindFirstChild("Head"):FindFirstChild("givemission").Enabled and v:FindFirstChild("CLIENTTALK") and v:FindFirstChild("Talk") and string.find(v.Talk.talk1.Value, "TRICK") and v.Talk:FindFirstChild("typ").Value == "halloweenevent" and v.Head.givemission.color.Image == candy then
+            				repeat wait()
+                			    toTarget(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position,v.HumanoidRootPart.Position,CFrame.new(v.HumanoidRootPart.Position+Vector3.new(0,-5,0)))
+        	        			v.CLIENTTALK:FireServer()
+        	        			wait(.2)
+        		        		v.CLIENTTALK:FireServer("accept")
+        		    	    until v:FindFirstChild("Head").givemission.Enabled == false or not candies
+        		        end
+                    end)
     			end
             else
                 print("max spins reached 500")
